d584042901abb7bbdd9ef66ef6129d5d4f2a3239
jurichar <jurichar@student.42.fr>
2023-06-18 23:21:10 +0200

    Add text to about component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,47 +1,57 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "./About.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const eventData = [
   {
     date: "2017",
-    title: "University Paris Diderot",
-    description: "Event description goes here.",
+    title: "University of Paris",
+    description: "> Computer Science\n> Mathematics\n> Algorithms\n> Basic web dev",
+    image: "src/assets/matrix_gif.gif",
   },
   {
     date: "2018",
     title: "Educational Engineer",
-    description: "Event description goes here.",
+    description: "> Pedagogical Skills\n> Programming Instruction\n> Basic Robotics",
+    image: "src/assets/matrix_gif.gif",
   },
   {
     date: "2020",
     title: "School 42 Paris",
-    description: "Event description goes here.",
+    description: "> Peer Learning\n> C, C++ Programming\n> FullStack Development\n> Project-based Learning",
+    image: "src/assets/matrix_gif.gif",
   },
   {
     date: "2022",
     title: "Software Engineer",
-    description: "Event description goes here.",
+    description: "> Implementation of UX\n> Qt Framework\n> Software Development\n> User Interface Design",
+    image: "src/assets/matrix_gif.gif",
   },
   {
     date: "2023",
     title: "DevOps Engineer",
-    description: "Event description goes here.",
+    description: "> Continuous Integration\n> Continuous Deployment\n> Infrastructure as Code\n> MLOps ",
+    image: "src/assets/matrix_gif.gif",
   },
 ];
 
-const Event = ({whichData}) => {
-        console.log(typeof whichData);
-    return (
-      <div className="event">
-        <div className="event-date">{eventData[whichData].date}</div>
-        <div className="event-card">
-          <span role="img" aria-label="rocket">
-            🚀
-          </span>
-          <h3>{eventData[whichData].title}</h3>
-          <p>{eventData[whichData].description}</p>
-        </div>
+const Event = ({ whichData }) => {
+  console.log(typeof whichData);
+  return (
+    <div className="event">
+      <div className="event-date">{eventData[whichData].date}</div>
+      <div className="event-card">
+        <img src={eventData[whichData].image} alt="About_gif" />
+        <h3>{eventData[whichData].title}</h3>
+        <p>{eventData[whichData].description.split('\n').map((line, i) => 
+            <span key={i}>
+              {line}
+              <br />
+            </span>
+          )}</p>
       </div>
-    );
+    </div>
+  );
 };
 
 const About = () => {
@@ -50,19 +60,21 @@ const About = () => {
       &#128679; About (in work) &#128679;
       <div id="about" className="About">
         <div className="timeline-container">
-          <div className="timeline"></div>
+          <div className="timeline">
+          <FontAwesomeIcon icon={faArrowRight} />
+          </div>
           <div className="events">
-                <div className="events_upper">
-                <Event whichData={0} />
-                <div id="middle_card">
-                        <Event whichData={1} />
-                </div>
+            <div className="events_upper">
+              <Event whichData={0} />
+              <div id="middle_card">
                 <Event whichData={2} />
-                </div>
-                <div className="events_lower">
-                <Event whichData={3} />
-                <Event whichData={4} />
-                </div>
+              </div>
+              <Event whichData={4} />
+            </div>
+            <div className="events_lower">
+              <Event whichData={1} />
+              <Event whichData={3} />
+            </div>
           </div>
         </div>
       </div>
