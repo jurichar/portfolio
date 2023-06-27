b006602b884e7a791afb07ccfaffe2af2d0694c1
jurichar <jurichar@student.42.fr>
2023-06-27 23:14:02 +0200

    add mobile for project

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,14 +1,12 @@
 import './Projects.scss';
 
-const Project = (title, description, image) => {
+const Project = (description, image) => {
     const content = {
-        title: title,
         description: description,
         image: image
     };
     return (
         <div className="Project">
-            <h1 className="Title"> { content.title } </h1>
             <div className="Picture">
                 <img src={ content.image } alt="Project_gif" />
             </div>
@@ -23,9 +21,9 @@ const Projects = () => {
     return (
         <header className="About-header">
             <div id="projects" className="Projects">
-                    { Project("Project 1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, diam ac scelerisque laoreet, enim libero imperdiet erat, ac tempus.", "/matrix_gif.gif") }
-                    { Project("Project 2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, diam ac scelerisque laoreet, enim libero imperdiet erat, ac tempus.", "/matrix_gif.gif") }
-                    { Project("Project 3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, diam ac scelerisque laoreet, enim libero imperdiet erat, ac tempus.", "/matrix_gif.gif") }
+                    { Project("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, diam ac scelerisque laoreet, enim libero imperdiet erat, ac tempus.", "/matrix_gif.gif") }
+                    { Project("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, diam ac scelerisque laoreet, enim libero imperdiet erat, ac tempus.", "/matrix_gif.gif") }
+                    { Project("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, diam ac scelerisque laoreet, enim libero imperdiet erat, ac tempus.", "/matrix_gif.gif") }
             </div>
         </header>
     );
