1972297122b213a52b4574a8148de87bab984133
Julien Richard <45456851+jurichar@users.noreply.github.com>
2023-08-30 15:03:20 +0200

    fix global and add gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import Contact from './components/Contact/Contact';
-import Projects from './components/Projects/Projects';
-import About from './components/About/About';
-import Techs from './components/Techs/Techs';
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+import Projects from "./components/Projects/Projects";
+import About from "./components/About/About";
+import Techs from "./components/Techs/Techs";
 import { aboutData } from "../src/data/about.json";
 import { projectsData } from "../src/data/projects.json";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="content">
-      <Home />
-      <About data={aboutData}/>
-      <Projects data={projectsData}/>
-      <Techs />
-      <Contact />
+        <Home />
+        <About data={aboutData} />
+        <Projects data={projectsData} />
+        <Techs />
+        <Contact />
       </div>
     </>
   );
