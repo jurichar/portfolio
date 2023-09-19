6c15e6538b24876ff13b6c2ca0a172d5996a8488
Julien Richard <45456851+jurichar@users.noreply.github.com>
2023-09-20 01:23:51 +0200

    try add vercel script

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.scss";
-import "./_variables.scss";
-import { inject } from "@vercel/analytics";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.scss';
+import './_variables.scss';
 
-inject();
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+      <App />
+  </React.StrictMode>,
+)
