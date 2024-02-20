7d41874b077770e05384003dee81e7614d3bd949
Julien RICHARD <julien.richard@betc.com>
2024-02-20 17:07:42 +0100

    Add react-svg dependency and new plus.svg icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 // Components/Navbar.tsx
 
-import React from 'react'
 import { Link } from 'react-router-dom'
+// import svg from '../assets/logo.svg'
+import { ReactSVG } from 'react-svg'
 
 function Navbar() {
   return (
     <nav className="fixed w-screen bg-black text-white">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
+      <ReactSVG src="/assets/icons/plus.svg" />
     </nav>
   )
 }
